322859a426b1ee2e94f85a08edaeace3d59234bb
Dito YP <killcoder212@gmail.com>
2019-09-11 07:15:00 +0700

    add update category

--- app/Http/Controllers/Api/Category/UpdateAction.php
+++ app/Http/Controllers/Api/Category/UpdateAction.php
@@ -2,10 +2,30 @@
 
 namespace App\Http\Controllers\Api\Category;
 
+use App\Models\Category;
+use App\Models\Repository\CategoryRepository\CategoryRepositoryInterface;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class UpdateAction extends Controller
 {
-    //
+    private $categoryRepository;
+
+    public function __construct(CategoryRepositoryInterface $categoryRepository)
+    {
+        $this->categoryRepository = $categoryRepository;
+    }
+
+    public function __invoke(Request $request, $id)
+    {
+        $category = Category::find($id);
+
+        if (is_null($category)) {
+            return response()->json(['error' => 'Product not found'], 500);
+        }
+
+        $this->categoryRepository->update($category, $request->all());
+
+        return response()->json(['success' => 'Product updated'], 200);
+    }
 }
